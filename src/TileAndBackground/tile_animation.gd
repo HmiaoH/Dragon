ae7b0194a357befb3f7b30482ae966cfa26d267d
HmiaoH <cjh15155968290@qq.com>
2025-08-03 21:24:12 +0800

    更新积分系统与完善地图移动 积分系统通过里程积分，细节详见文档 地图的移动现在不会有割裂感，更丝滑且最大速度更快

--- src/TileAndBackground/tile_animation.gd
+++ src/TileAndBackground/tile_animation.gd
@@ -6,14 +6,15 @@ extends Node2D
 @onready var tile_map_layer_1: TileMapLayer = $TileMapLayer
 @onready var tile_map_layer_2: TileMapLayer = $TileMapLayer2
 
-const MAX_SPEED: int = -200
+const MAX_SPEED: int = -300
 
 var tile_map_layer_size: int
 var tile_size: int
 
 func _ready() -> void:
     tile_size = tile_map_layer_1.tile_set.tile_size.x
-    tile_map_layer_size = tile_map_layer_1.get_used_rect().size.x * tile_size
+    var used_rect_size = tile_map_layer_1.get_used_rect().size.x
+    tile_map_layer_size = (used_rect_size - 2) * tile_size
 
 func _physics_process(delta: float) -> void:
     tile_map_layer_1.position.x += animation_speed * delta
--- src/World/world.gd
+++ src/World/world.gd
@@ -1,12 +1,28 @@
 class_name World
 extends Node2D
 
+var score: float = 0.0:
+	set(value):
+		score = value
+		on_score_changed.emit(value)
+var score_incremental: float = 0.5
+var obstacles: Array[Area2D] = []
+
+signal on_score_changed(value)
+
 @onready var bg_animation: BgAnimation = $BgAnimation
 @onready var tile_animation: TileAnimation = $TileAnimation
 
 func _on_change_state_timeout() -> void:
 	bg_animation.change_speed(5)
 	tile_animation.change_speed(10)
+
 	print("chang_velocity")
-	if bg_animation.animation_speed == bg_animation.MAX_SPEED:
+	if bg_animation.animation_speed <= bg_animation.MAX_SPEED:
 		$ChangeState.queue_free()
+
+	score_incremental += 0.5
+
+
+func _on_add_score_timer_timeout() -> void:
+	score += score_incremental
